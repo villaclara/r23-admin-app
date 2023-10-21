0402b9a566017fd5d68aa30400b2d2d2b17a91aa
villaclara <villaclara1231@gmail.com>
2023-10-22 00:53:41 +0300

    fixing dif

--- AdminApp.WASM/Program.cs
+++ AdminApp.WASM/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddScoped(delegate (IServiceProvider sp)
 	{
 		BaseAddress = new Uri("http://localhost:5117/")
 		//BaseAddress = new Uri("https://testwasmblazor.bsite.net/")
+		//BaseAddress = new Uri("https://2eb9-217-196-161-189.ngrok-free.app")
 	};
 });
 
