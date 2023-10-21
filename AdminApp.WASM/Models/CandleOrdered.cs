7de51c6885c326027f93b07ee4510d91e70cb39d
villaclara <villaclara1231@gmail.com>
2023-10-21 14:12:53 +0300

    add candle in new order done

--- AdminApp.WASM/Models/CandleOrdered.cs
+++ AdminApp.WASM/Models/CandleOrdered.cs
@@ -6,7 +6,11 @@
 		public string Name { get; set; } = null!;
 		public int Quantity { get; set; }
 		public decimal Price { get; set; }
-		public decimal TotalSum { get; set; }
+
+
+		public decimal TotalSum { 
+			get => Quantity * Price; 
+		}  
 		public int WaxNeededGramm { get; set; }
 		public int WickDiameterCM { get; set; }
 	}
