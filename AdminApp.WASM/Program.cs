402022d2a220f604a442c425470815eca31b3bb3
villaclara <villaclara1231@gmail.com>
2023-10-27 16:51:31 +0300

    done

--- AdminApp.WASM/Program.cs
+++ AdminApp.WASM/Program.cs
@@ -17,8 +17,8 @@ builder.Services.AddScoped(delegate (IServiceProvider sp)
 {
 	return new HttpClient
 	{
-		BaseAddress = new Uri("http://localhost:5117/")
-		//BaseAddress = new Uri("https://testwasmblazor.bsite.net/")
+		//BaseAddress = new Uri("http://localhost:5117/")
+		BaseAddress = new Uri("https://testwasmblazor.bsite.net/")
 		//BaseAddress = new Uri("https://2eb9-217-196-161-189.ngrok-free.app")
 	};
 });
