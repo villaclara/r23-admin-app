9a309553ae573eb4cb6fe42aac7c4167be532721
villaclara <villaclara1231@gmail.com>
2023-10-27 23:35:01 +0300

    add format prices n padding for add canlde button

---
+++ AdminApp.WASM/Application/Utility/PriceFormatExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AdminApp.WASM.Application.Utility
+{
+	public static class PriceFormatExtensions
+	{
+		public static string ToZeroNumbersAfterComma(this decimal value) => value.ToString("N0");
+	}
+}
