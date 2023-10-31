94d27bd1cf45fec742a33e2076e0178edeef57a5
villaclara <villaclara1231@gmail.com>
2023-11-01 00:20:42 +0200

    started doing notes

---
+++ AdminApp.WASM/Application/Services/NoteHandlerService.cs
@@ -0,0 +1,33 @@
+﻿using AdminApp.WASM.Application.Interfaces;
+using AdminApp.WASM.Models.ViewModels;
+
+namespace AdminApp.WASM.Application.Services
+{
+    public class NoteHandlerService : ICRUDService<NoteVM>
+    {
+        public Task<bool> DeleteItemFromURLAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteVM?> GetItemAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<NoteVM>> GetItemsAsListAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PostItemAsync(string url, NoteVM item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PutItemAsync(string url, NoteVM item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdminApp.WASM/Models/ViewModels/NoteVM.cs
@@ -0,0 +1,10 @@
+﻿namespace AdminApp.WASM.Models.ViewModels
+{
+    public class NoteVM
+    {
+        public int Id { get; set; }
+        public bool IsDone { get; set; }
+        public string NoteText { get; set; } = null!;
+        public DateOnly NoteDate { get; set; }
+    }
+}
