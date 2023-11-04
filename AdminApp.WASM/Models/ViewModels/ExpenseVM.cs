830061764c11262aa1c1a23720d2e7d39631b2e0
villaclara <villaclara1231@gmail.com>
2023-11-04 16:01:39 +0200

    backend done

--- AdminApp.WASM/Models/ViewModels/ExpenseVM.cs
+++ AdminApp.WASM/Models/ViewModels/ExpenseVM.cs
@@ -1,10 +1,15 @@
-﻿namespace AdminApp.WASM.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminApp.WASM.Models.ViewModels
 {
 	public class ExpenseVM
 	{
 		public int Id { get; set; }
+		[Required]
 		public string Description { get; set; } = null!;
 		public DateOnly Date { get; set; }
+		[Required]
+		[Range(1, double.MaxValue)]
 		public decimal Cost { get; set; }
 	}
 }
