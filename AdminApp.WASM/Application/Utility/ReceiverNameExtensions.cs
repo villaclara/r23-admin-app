f9c693fb8a5415ff434e7b0317c2f752fc629ed2
villaclara <villaclara1231@gmail.com>
2023-11-12 04:07:14 +0200

    orderlistNewDesignWIP

---
+++ AdminApp.WASM/Application/Utility/ReceiverNameExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace AdminApp.WASM.Application.Utility
+{
+	public static class ReceiverNameExtensions
+	{
+		public static string ToOnlyFirstLastName(this string fullname)
+		{
+			var names = fullname.TrimEnd().Split(' ');
+			if (names.Length == 3)
+			{
+				return names[0] + " " + names[1];
+			}
+
+			return fullname;
+		}
+	}
+}
