796c8154f3647315823365fee53213cd7ecd8292
villaclara <villaclara1231@gmail.com>
2023-12-14 10:23:14 +0200

    first test test

---
+++ Road23.AdminApp.Tests/ComponentsTests/CandleComponentsTests/NewCandleComponent.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Bunit;
+using AdminApp.WASM.Components.Misc;
+
+namespace Road23.AdminApp.Tests;
+
+public class NewCandleComponent : TestContext
+{
+
+    [Fact]
+    public void AddCandleButtonRendersCorrectly()
+    {
+        // Act
+        var cut = RenderComponent<ButtonWithSpinnerAction>();
+
+        //Assert
+        cut.MarkupMatches("""<button class="@ButtonCss" disabled="@IsButtonPressed"><span class="@SetButtonCssSpinner(IsButtonPressed)" aria-hidden="true"></span><span>@ButtonText</span></button>""");
+    }
+
+}
